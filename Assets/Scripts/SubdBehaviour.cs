2d7a1f706780ee017a8dfe6caa9ed9c06e0e83e3
wenqian157 <yang@arch.ethz.ch>
2023-01-31 11:53:51 +0100

    init

---
+++ Assets/Scripts/SubdBehaviour.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HD;
+using System.Reflection;
+using System;
+
+[ExecuteInEditMode]
+public class SubdBehaviour : MonoBehaviour
+{
+    public Material material;
+    [Range(0.1f,5)]
+    public float extrudeHeight = 1f;
+    [Range(0,12)]
+    public int iteration = 2;
+    private Mesh mesh;
+    private HDMesh hdMesh;
+
+    void Start()
+    {
+        Debug.Log("start is called");
+        InitMesh();
+        List<string> functionStrings = new List<string>(){ "CustomizedBehaviour, CustomizedB" };
+        //mesh.MarkDynamic();
+    }
+
+    // this will be called everytime something changes in inspector
+    private void OnValidate()
+    {
+        Debug.Log("Inspector causes this Update");
+        InitHDMesh();
+        for (int i = 0; i < iteration; i++)
+        {
+            MethodInfo theMethod = GetType().GetMethod("BehaviourB");
+            theMethod.Invoke(this, null);
+            //CustomizedBehaviour();
+        }
+
+        Debug.Log(hdMesh.FacesCount());
+        hdMesh.FillUnityMesh(mesh);
+    }
+
+    public void InitHDMesh()
+    {
+        HDMesh newMesh = new HDMesh();
+        Color color = Color.white;
+        HDMeshFactory.AddBox(newMesh, 0, 0, 0, 1, 1, 1, color);
+        //HDMeshFactory.AddTriangle(newMesh, 0, 0, 0, 1, 0, 0, 0, 1, 0, color);
+        hdMesh = newMesh;
+        //return hdMesh;
+    }
+
+    public void BehaviourA()
+    {
+        hdMesh = HDMeshSubdivision.subdivide_mesh_extrude(hdMesh, extrudeHeight);
+    }
+    public void BehaviourB()
+    {
+        hdMesh = HDMeshSubdivision.subdivide_mesh_grid(hdMesh, 2, 2);
+    }
+
+    public void CustomizedBehaviour()
+    {
+        HDMesh newMesh = new HDMesh();
+        foreach(var face in hdMesh.Faces) //list of index
+        {
+            List<Vector3[]> new_faces_vertices = HDMeshSubdivision.subdivide_face_extrude(hdMesh, face, extrudeHeight);
+            foreach(var face_vertices in new_faces_vertices)
+            {
+                newMesh.AddFace(face_vertices);
+            }
+        }
+
+        hdMesh = newMesh;
+    }
+
+    private void InitMesh()
+    {
+        // init mesh filter
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (null == meshFilter)
+        {
+            meshFilter = this.gameObject.AddComponent<MeshFilter>();
+        }
+        mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        meshFilter.mesh = mesh;
+
+        // init mesh renderer
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        if (renderer == null)
+        {
+            renderer = this.gameObject.AddComponent<MeshRenderer>();
+        }
+        renderer.material = material;
+    }
+
+}
---
+++ Assets/Scripts/VoxelBehaviour.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HD;
+using System;
+using Random = UnityEngine.Random;
+
+[ExecuteInEditMode]
+public class VoxelBehaviour : MonoBehaviour
+{
+    public Material material;
+
+    public int nX = 100;
+    public int nY = 100;
+    public int nZ = 100;
+    [Range(0, 50)]
+    public float radius = 0;
+
+    private HDGrid<bool> grid;
+    private Mesh mesh;
+    private HDMesh hdMesh;
+
+    void Start()
+    {
+        Debug.Log("start is called");
+        InitMesh();
+    }
+
+    private void OnValidate()
+    {
+        Debug.Log("Inspector causes this Update");
+        InitGrid();
+        SphereGrid(new Vector3(20, 20, 20), radius);
+        hdMesh = VoxelMesh(Color.red);
+        //hdMesh.SeparateVertices();
+        hdMesh.FillUnityMesh(mesh);
+    }
+
+    private void InitGrid()
+    {
+        grid = new HDGrid<bool>(nX, nY, nZ);
+    }
+
+    private void RandomGrid()
+    {
+        for (int i = 0; i < grid.Count; i++)
+        {
+            grid[i] = Random.value > 0.5;
+        }
+    }
+
+    private void SphereGrid(Vector3 centerPt, float radius)
+    {
+        float radiusSquare = Mathf.Pow(radius, 2);
+        for (int i = 0; i < grid.Count; i++)
+        {
+            float dx = Mathf.Pow(grid.getX(i) - centerPt.x, 2);
+            float dy = Mathf.Pow(grid.getY(i) - centerPt.y, 2);
+            float dz = Mathf.Pow(grid.getZ(i) - centerPt.z, 2);
+
+            float dSquare = dx + dy + dz;
+            if (dSquare <= radiusSquare)
+            {
+                grid[i] = true;
+            }
+        }
+    }
+
+    private HDGrid<bool> CustomizedGrid(int nX, int nY, int nZ)
+    {
+        HDGrid<bool> grid = new HDGrid<bool>(nX, nY, nZ);
+        // customization
+        for (int x = 10; x < 20; x++)
+        {
+            for (int y = 0; y < 30; y++)
+            {
+                for (int z = 10; z < 25; z++)
+                {
+                    grid[x, y, z] = true;
+                }
+            }
+        }
+        return grid;
+    }
+
+    private HDGrid<bool> GyroidGrid(int nX, int nY, int nZ)
+    {
+        throw new NotImplementedException();
+    }
+
+    public HDMesh VoxelMesh(Color color)
+    {
+        HDMesh myMesh = new HDMesh();
+        for (int x = 0; x < nX; x++)
+        {
+            for (int y = 0; y < nY; y++)
+            {
+                for (int z = 0; z < nZ; z++)
+                {
+                    if (grid[x, y, z])
+                    {
+                        Color colorFacade = color;
+                        HDMeshFactory.AddBox(myMesh, x, y, z, x + 1, y + 1f, z + 1, colorFacade);
+
+                    }
+                }
+            }
+        }
+        return myMesh;
+    }
+    private void InitMesh()
+    {
+        // init mesh filter
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (null == meshFilter)
+        {
+            meshFilter = this.gameObject.AddComponent<MeshFilter>();
+        }
+        mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        meshFilter.mesh = mesh;
+
+        // init mesh renderer
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        if (renderer == null)
+        {
+            renderer = gameObject.AddComponent<MeshRenderer>();
+        }
+        renderer.material = material;
+    }
+}
